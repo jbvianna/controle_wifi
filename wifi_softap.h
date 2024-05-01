b1d87392ee63a577cc9b0444388a2194adeb0fc1
João Vianna <jvianna@gmail.com>
2024-05-01 08:32:57 -0300

    Delete wifi_softap.h

--- wifi_softap.h
+++
@@ -1,9 +0,0 @@
-/** @file wifi_softap.h - Inicia Wifi no modo soft Access Point.
- */
-#pragma once
-
-/** Iniciar serviço de Wifi no modo Soft AP (Access Point) 
-
-    @param ssid Identificador do Access Point
- */
-void wifi_init_softap(const char *ssid);
